ce0944faf9658b48858cbc2ee09b4632c3d37add
romuro-pauliv <romulopauliv@bk.ru>
2024-06-26 18:34:48 -0300

    added regression test

--- app/__main__.py
+++ app/__main__.py
@@ -21,30 +21,42 @@ df      : pd.DataFrame = reader.read("MetroPT3(AirCompressor)")
 from analysis.daily_periodicity import SplitbyPeriod
 from datetime import timedelta, datetime
 
+
 a: SplitbyPeriod = SplitbyPeriod("Oil_temperature", df, timedelta(hours=2))
-a.global_datetime_max = datetime(2020, 2, 10, 0, 0, 0)
+a.global_datetime_max = datetime(2020, 2, 4, 0, 0, 0)
 a.run()
 x, y, t = a.get_periods()
 
+
+
+
+# | WorkBench |-------------------------------------------------------------------------------------------------------|
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.axes import Axes
+import numpy as np
+
+from sklearn.ensemble import GradientBoostingRegressor
 
 for i in range(len(x)):
     FIG: tuple[Figure, tuple[Axes]] = plt.subplots(nrows=2, figsize=(16, 8))
     
+    x_: np.ndarray = x[i]
+    y_: np.ndarray = y[i]
     
-    mean: pd.Series = pd.Series(y[i]).rolling(1000).mean()
-    std : pd.Series = pd.Series(y[i]).rolling(1000).std()
+    clf: GradientBoostingRegressor = GradientBoostingRegressor(n_estimators=500, max_depth=10)
+    clf.fit(x_.reshape(-1, 1), y_)
+    y_p: np.ndarray = clf.predict(x_.reshape(-1, 1))
     
-    FIG[1][0].scatter(t[i], y[i], s=0.2)
-    FIG[1][0].plot(t[i], mean, color="red")
-    FIG[1][1].scatter(t[i], (y[i]-mean), s=0.2)
-    FIG[1][0].plot(t[i], (mean+3*std), color="red", alpha=0.2, linestyle="dashed")
-    FIG[1][0].plot(t[i], (mean-3*std), color="red", alpha=0.2, linestyle="dashed")
+    
+    FIG[1][0].scatter(x_, y_, s=0.2)
+    FIG[1][0].plot(x_, y_p, color="red", linewidth=0.5)
+    
+    
     
     
     FIG[1][0].grid(True, "both")
     FIG[1][1].grid(True, "both")
+    
     plt.show()
     plt.clf()
